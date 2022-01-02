8384eedb97709316a9fee528486f838df51fc5bc
hraj55654 <94531887+hraj55654@users.noreply.github.com>
2022-01-02 10:07:46 +0530

    Add files via upload

---
+++ 22 assignment/pusharray.js
@@ -0,0 +1,15 @@
+var arr = [];
+
+var i = prompt("Enter first number");
+i = parseInt(i);
+arr.push(i);
+
+i = prompt("Enter second number");
+i = parseInt(i);
+arr.push(i);
+
+
+i = prompt("Enter third number");
+i = parseInt(i);
+arr.push(i);
+alert(arr);
